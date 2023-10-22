8cbcddf97ba7e7291440404ea565debc4768b42f
LAVINIA-ADELA DUCA <lavinia.duca@stud.ubbcluj.ro>
2023-10-22 23:39:58 +0300

    Add project files.

---
+++ Migrations/20231022200259_Author.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Duca_Lavinia_Lab2.Migrations
+{
+    public partial class Author : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Authors_AuthorID",
+                table: "Book");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorID",
+                table: "Book",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.ID);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Author_AuthorID",
+                table: "Book",
+                column: "AuthorID",
+                principalTable: "Author",
+                principalColumn: "ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Author_AuthorID",
+                table: "Book");
+
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorID",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.ID);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Authors_AuthorID",
+                table: "Book",
+                column: "AuthorID",
+                principalTable: "Authors",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Pages/Publishers/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Duca_Lavinia_Lab2.Data;
+using Duca_Lavinia_Lab2.Models;
+
+namespace Duca_Lavinia_Lab2.Pages.Publishers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Duca_Lavinia_Lab2.Data.Duca_Lavinia_Lab2Context _context;
+
+        public CreateModel(Duca_Lavinia_Lab2.Data.Duca_Lavinia_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Publisher == null || Publisher == null)
+            {
+                return Page();
+            }
+
+            _context.Publisher.Add(Publisher);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
